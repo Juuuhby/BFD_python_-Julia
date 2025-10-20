ec9b456c849ff71367023788d61f03eecef7c2cd
Juuuhby <julia010706@gmail.com>
2025-10-20 20:10:35 -0300

    teste

--- Modulo 3/aula.py
+++ Modulo 3/aula.py
@@ -5,3 +5,14 @@ con = sqlite3.connect("tabela.db")
 cursor = con.cursor()
 
 print("Bancos de Dados criado!")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS clientes(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               nome TEXT NOT NULL,
+               cpf TEXT NOT NULL,
+               telefone TEXT NOT NULL,
+               estado TEXT NOT NULL); """)
+
+print("Tabela criada com sucesso!")
+
+con.commit()
