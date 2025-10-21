3dd50209f03031dc7072a034113e4c19cf2ca414
Juuuhby <julia010706@gmail.com>
2025-10-20 21:19:22 -0300

    ex1

---
+++ Modulo 1/prova.py
@@ -0,0 +1,16 @@
+# #Atividade número 1
+# print("Conversor de temperaturas")
+# print("1. Celsius para Fahrenheit")
+# print("2. Fahrenheit para Celsius")
+
+# number = int(input("Escolha uma opção:"))
+
+# if number == 1:
+#     c = float(input("Temperatura em celsius: "))
+#     calc = ( c * 9 / 5) + 32
+#     print(f"A conversão de {c}° C para fahrenheit é {calc:.1f}° F.")
+# elif number == 2:
+#     f = float(input("Temperatura em fahrenheit: "))
+#     calc2 = ( f - 32) * 5 / 9
+#     print(f"A conversão de {f}° F para celsius é {calc2:.1f}° C.")
+
